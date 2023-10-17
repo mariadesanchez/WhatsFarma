4d8bdf0f2de127637d7f643720882ba3ed2b9275
mariade_snchz <calder07@gmail.com>
2023-10-17 15:14:14 -0300

    toggle

--- src/components/pages/itemlist/ItemListContainer.jsx
+++ src/components/pages/itemlist/ItemListContainer.jsx
@@ -193,17 +193,14 @@ import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 </button>
 
 {modalIsOpen && (
-  <div id="categorias" className="lg:col-span-1" style={{ marginTop: '1rem', position: 'absolute', top: 0, left: '120px',zIndex:'100' }}>
-      <div
-    className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 text-black"
-    onClick={toggleModal} // Cerrar el modal cuando se hace clic fuera de él
-  >
-    <div
-      className="lg:col-span-1 p-4 rounded-lg shadow-lg"
-      style={{ marginTop: '1rem', position: 'absolute', top: 0, left: '120px', zIndex: '100' }}
-    >
-
-    </div>
+ <div
+ className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 text-black"
+ onClick={toggleModal} // Cerrar el modal cuando se hace clic fuera de él
+>
+ <div
+   className="lg:col-span-1 p-4 rounded-lg shadow-lg"
+   style={{ marginTop: '1rem', position: 'absolute', top: 0, left: '120px', zIndex: '100' }}
+ >    
     <h2 className="text-2xl">CATEGORIAS:</h2>
     <IconButton onClick={handleDeleteAllCategories}>
       <DeleteForeverIcon color="primary" />
