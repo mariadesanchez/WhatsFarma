5b7cad84e579ed2567656fd103ec0ed70a38c20b
mariade_snchz <calder07@gmail.com>
2023-10-17 14:32:29 -0300

    delete modal

--- src/components/pages/itemlist/ItemListContainer copy.jsx
+++ src/components/pages/itemlist/ItemListContainer copy.jsx
@@ -1,44 +1,35 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-undef */
 /* eslint-disable react-hooks/exhaustive-deps */
-
 import { useEffect, useState,useContext } from "react";
 import { db } from "../../../firebaseConfig";
 // eslint-disable-next-line no-unused-vars
 import {where , query,getDocs, collection,addDoc,deleteDoc} from "firebase/firestore";
 import { Link } from "react-router-dom";
 // eslint-disable-next-line no-unused-vars
-
-
 import corazon from '../../../../src/images/corazon.png';
 import corazonRojo from '../../../../src/images/corazon-rojo.png';
 // eslint-disable-next-line no-unused-vars
 import { AuthContext } from "../../../context/AuthContext"
-// import '../itemlist/ItemListContainer.scss'
-
 import { usecontextGlobal } from '../../../context/GlobalContext'
+import {  IconButton } from "@mui/material";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 
-
-   
-
-const ItemListContainer= () => {
-
+  const ItemListContainer= () => {
   const {productState, productDispatch} = usecontextGlobal()
-
   const {user} = useContext(AuthContext)
   // localStorage.clear();
   const [favoritos, setFavoritos] = useState([]);
   const [productos, setProductos] = useState([]);
   const [productosFavNoFav, setProductosFavNoFav] = useState([]);
   const [categorias, setCategorias] = useState([]);
-  // const [categoriasSeleccionadas, setCategoriasSeleccionadas] = useState([]);
   const [productosPorCategorias,setProductosPorCategorias]=useState([])
-  const [seleccionarTodas, setSeleccionarTodas] = useState(true);
+  const [seleccionarTodas, setSeleccionarTodas] = useState(false);
 
+  // traer todos los favoritos de este usuario por su email
 
-
-  // eslint-disable-next-line no-undef, no-unused-vars
   useEffect(() => {
     let refCollection = collection(db, "favoritos");
     const favoritosQuery = query(refCollection, where("email", "==", user.email));
@@ -55,7 +46,7 @@ const ItemListContainer= () => {
   }, [favoritos]);
 
 
-
+// traer todos los productos
   useEffect(() => {
     let refCollection = collection(db, "products");
     getDocs(refCollection)
@@ -69,6 +60,7 @@ const ItemListContainer= () => {
       .catch((err) => console.log(err));
   }, [productos]);
 
+// traer todas las categorias
 
    useEffect(() => {
     let refCollection = collection(db, "categorias");
@@ -97,7 +89,10 @@ const ItemListContainer= () => {
       productDispatch({type: 'CATEGORIAS_SELECCIONADAS', payload:nuevasCategoriasSeleccionadas })
 
     }
-  };
+  }
+
+  
+
   
   useEffect(() => {
     // Filtra los productos por categorías seleccionadas
@@ -108,27 +103,25 @@ const ItemListContainer= () => {
   }, [productState.categoriasSeleccionadas, productosFavNoFav]);
 
   
-  
   const handleSeleccionarTodasChange = () => {
+    // Invierte el valor actual de seleccionarTodas (true a false o viceversa)
     setSeleccionarTodas(!seleccionarTodas);
   
-    // Si se marca "Seleccionar todas", establece todas las categorías en el estado categoriasSeleccionadas.
     if (!seleccionarTodas) {
+      // Si se marca "Seleccionar todas", establece todas las categorías en el estado categoriasSeleccionadas.
       const todasLasCategorias = categorias.map((categoria) => ({
         id: categoria.id,
         title: categoria.title,
       }));
-      // setCategoriasSeleccionadas(todasLasCategorias);
-            productDispatch({type: 'SELECCIONAR_TODAS', todasLasCategorias })
-
+      // Actualiza el estado con las nuevas categorías seleccionadas
+      productDispatch({ type: 'CATEGORIAS_SELECCIONADAS', payload: todasLasCategorias });
     } else {
       // Si se desmarca, borra todas las categorías seleccionadas.
-      setCategoriasSeleccionadas([]);
+      // Actualiza el estado para borrar todas las categorías seleccionadas
+      productDispatch({ type: 'CATEGORIAS_SELECCIONADAS', payload: [] });
     }
-  };
+  }
   
-  
-
   useEffect(() => {
     // Filtra los productos que son favoritos
     const productosFavoritosTemp = productos.map((producto) => {
@@ -180,87 +173,157 @@ const ItemListContainer= () => {
       console.error('Error al actualizar favoritos:', error);
     }
   }
-
+  const handleDeleteAllCategories = () => {
+    // Realiza la acción que deseas ejecutar, en este caso, la llamada a productDispatch
+    productDispatch({ type: 'CATEGORIAS_SELECCIONADAS', payload: [] });
+  };
   return (
     
-<div id='categoriasCards' className="flex justify-center">
-          
-          <div id="categorias" className="lg:col-span-1" style={{ marginTop: '1rem',display:'inline-block' }}>
+    <div id='categoriasCards' className="flex justify-center" style={{ marginTop: '100px', position: 'relative' }}>
+  <div id="categorias" className="lg:col-span-1" style={{ marginTop: '1rem', position: 'absolute', top: 0, left: '120px',zIndex:'100' }}>
+    <h2 className="text-2xl">CATEGORIAS:</h2>
+    <IconButton onClick={handleDeleteAllCategories}>
+      <DeleteForeverIcon color="primary" />
+    </IconButton>
+   
 
-        <h4>Categorias:</h4>
-        <label style={{ fontSize: '30px' }}>
+    {categorias.map((categoria) => (
+      <div key={categoria.id}>
+        <label className="text-2xl">
           <input
+            style={{
+              width: '20px',
+              height: '20px',
+            }}
             type="checkbox"
-            value="todos"
-            checked={seleccionarTodas}
-            onChange={handleSeleccionarTodasChange} />
-          Todas
+            value={categoria.id}
+            checked={productState.categoriasSeleccionadas.some(c => c.id === categoria.id)}
+            onChange={() => handleCheckboxChange(categoria.id, categoria.title)}
+          />
+          {categoria.title}
         </label>
-        <UserAvatar name="JS" /> 
-        {categorias.map((categoria) => (
-          <div key={categoria.id}>
-            <label style={{ fontSize: '30px' }}>
-              <input
-                type="checkbox"
-                value={categoria.id}
-                checked={productState.categoriasSeleccionadas.some(c => c.id === categoria.id)}
-                onChange={() => handleCheckboxChange(categoria.id, categoria.title)} />
-              {categoria.title}
-            </label>
-          </div>
-        ))}
       </div>
+    ))}
+  </div>
+
+  <div id="cards" className="lg:col-span-1" style={{ display: 'inline-block' }}>
     
     
-      {seleccionarTodas&& (
-    <div id="cards" className="lg:col-span-1" style={{display:'inline-block'}}>
+    <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4" style={{ gridAutoRows: "auto" }}>
 
-        <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
-          <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      { productosPorCategorias.length == 0 &&
+  productosFavNoFav.map((product) => {
+    return (
+      <Link to={`/itemDetail/${product.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+      <div id='FavNoFav' key={product.id} className="relative overflow-hidden bg-gray-200 rounded shadow-md hover:shadow-xl transform hover:-translate-y-2 transition duration-300">
+        <img
+            className="w-full h-200 rounded-lg object-cover"
 
-            {productState.categoriasSeleccionadas.length > 0 &&
-              productosPorCategorias.map((product) => {
-                return (
-                <div key={product.id} className="relative overflow-hidden bg-gray-200 rounded">
-                    <img
-                  className="w-full md:w-96 h-80 md:h-96 rounded-lg object-cover md:!rounded-none md:!rounded-l-lg"
-                   src={product.image}
-                      alt="" />
-                    <div className="flex flex-col justify-center items-center p-6">
-                      <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
-                        {product.title}
-                      </h5>
-                      <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                        Precio: {product.unit_price}
-                      </p>
-                      <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                        Stock: {product.stock}
-                      </p>
-                      <Link to={`/itemDetail/${product.id}`}>Ver detalle</Link>
-
-                      <button
-                        type="button"
-                        id="toggleButton"
-                        onClick={() => actualizarFavoritos(product.id)}
-                        href="#"
-                        className="inline-block rounded bg-primary px-8 py-3 text-base font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                        data-te-ripple-init
-                        data-te-ripple-color="light"
-                      >{product.fav ?
-                        (<><img src={corazon} id={product.id} style={{ width: '40px', height: '40px', display: 'none' }} /><img src={corazonRojo} id={product.id + 1} style={{ width: '40px', height: '40px', display: 'block' }} /></>)
-                        :
-                        (<><img src={corazon} id={product.id} style={{ width: '40px', height: '40px', display: 'block' }} /><img src={corazonRojo} id={product.id + 1} style={{ width: '40px', height: '40px', display: 'none' }} /></>)}
-                      </button>
-                    </div>
-                  </div>
-
-                );
-              })}
-          </div>
-        </div>  
+          src={product.image}
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-center p-6">
+          <h5 style={{ fontSize: '14px', lineHeight: '1.3' }}>
+            {product.title}
+          </h5>
+          <h3 style={{ fontSize: '18px',lineHeight: '1.2', fontWeight: 'bold' }}>
+            $ {product.unit_price}
+          </h3>
+          <h4 style={{ fontSize: '12px', lineHeight: '1.2', fontWeight: 'bold',color:'green' }}>
+            Stock: {product.stock}
+          </h4>
+          <button
+            type="button"
+            id="toggleButton"
+            onClick={() => actualizarFavoritos(product.id)}
+            href="#"
+            className="absolute top-2 right-2 rounded bg-primary w-auto h-auto p-1 text-base font-medium uppercase leading-normal text-white shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition duration-300 focus:bg-primary-600 focus:shadow-2xl focus:outline-none focus:ring-2 focus:ring-primary dark:shadow-md dark:hover:shadow-2xl dark:focus:shadow-2xl dark:ring-primary"
+            data-te-ripple-init
+            data-te-ripple-color="light"
+          >
+            {product.fav ?
+              (
+                <>
+                  <img src={corazon} id={product.id} style={{ width: '20px', height: '20px', display: 'none' }} />
+                  <img src={corazonRojo} id={product.id + 1} style={{ width: '20px', height: '20px', display: 'block' }} />
+                </>
+              )
+              :
+              (
+                <>
+                  <img src={corazon} id={product.id} style={{ width: '20px', height: '20px', display: 'block' }} />
+                  <img src={corazonRojo} id={product.id + 1} style={{ width: '20px', height: '20px', display: 'none' }} />
+                </>
+              )
+            }
+          </button>
+        </div>
       </div>
-        )}
+    </Link>
+    
+    );
+  })
+}
+        
+        
+        
+        {(productState.categoriasSeleccionadas.length > 0) &&
+          productosPorCategorias.map((product) => {
+            return (
+              <Link to={`/itemDetail/${product.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+  <div id='FavNoFav' key={product.id} className="relative overflow-hidden bg-gray-200 rounded shadow-md hover:shadow-xl transform hover:-translate-y-2 transition duration-300">
+    <img
+        className="w-full h-200 rounded-lg object-cover"
+      src={product.image}
+      alt=""
+    />
+    <div className="flex flex-col justify-center items-center p-6">
+      <h5 style={{ fontSize: '14px', lineHeight: '1.3' }}>
+        {product.title}
+      </h5>
+      <h3 style={{ fontSize: '18px',lineHeight: '1.2', fontWeight: 'bold' }}>
+        $ {product.unit_price}
+      </h3>
+      <h4 style={{ fontSize: '12px', lineHeight: '1.2', fontWeight: 'bold',color:'green' }}>
+        Stock: {product.stock}
+      </h4>
+      <button
+        type="button"
+        id="toggleButton"
+        onClick={() => actualizarFavoritos(product.id)}
+        href="#"
+        className="absolute top-2 right-2 rounded bg-primary w-auto h-auto p-1 text-base font-medium uppercase leading-normal text-white shadow-md hover:shadow-2xl transform hover:-translate-y-2 transition duration-300 focus:bg-primary-600 focus:shadow-2xl focus:outline-none focus:ring-2 focus:ring-primary dark:shadow-md dark:hover:shadow-2xl dark:focus:shadow-2xl dark:ring-primary"
+        data-te-ripple-init
+        data-te-ripple-color="light"
+      >
+        {product.fav ?
+          (
+            <>
+              <img src={corazon} id={product.id} style={{ width: '20px', height: '20px', display: 'none' }} />
+              <img src={corazonRojo} id={product.id + 1} style={{ width: '20px', height: '20px', display: 'block' }} />
+            </>
+          )
+          :
+          (
+            <>
+              <img src={corazon} id={product.id} style={{ width: '20px', height: '20px', display: 'block' }} />
+              <img src={corazonRojo} id={product.id + 1} style={{ width: '20px', height: '20px', display: 'none' }} />
+            </>
+          )
+        }
+      </button>
+    </div>
+  </div>
+</Link>
+
+            );
+          })}
       </div>
+    </div>
+  </div>
+</div>
+
     );
   };
 export default ItemListContainer;
