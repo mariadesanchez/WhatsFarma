e68af76853bf01317f6369a8cc30e3fe70c3c4c3
mariade_snchz <calder07@gmail.com>
2023-12-12 23:28:23 -0300

    image min

--- src/components/pages/Whatsapp.jsx
+++ src/components/pages/Whatsapp.jsx
@@ -8,25 +8,26 @@ import React, { useEffect } from 'react';
 const Whatsapp = () => {
   
     let storedOrderId = localStorage.getItem("order");
-    let orderData = {};
-    
-    try {
-      orderData = JSON.parse(storedOrderId);
-    } catch (error) {
-      console.error('Error al parsear el objeto de la orden:', error);
-    }
-    
-    // Extraer solo las propiedades necesarias y formatear el mensaje con imágenes en miniatura
-    const formattedMessage = orderData.items.map(item => (
-      `*[ ${item.image}]*\n💰 *Precio:* ${item.unit_price}\n🔢 *Cantidad:* ${item.quantity}\n\n`
-    ));
-    
-    // Crear el enlace de WhatsApp con el mensaje formateado
-    const encodedMessage = encodeURIComponent(`*Detalles del Pedido:*\n\n${formattedMessage.join('')}`);
-    const whatsappLink = `https://api.whatsapp.com/send?phone=5492213602683&text=${encodedMessage}`;
-    
-    // Abrir el enlace de WhatsApp en una nueva ventana o pestaña
-    window.open(whatsappLink, '_blank');
+let orderData = {};
+
+try {
+  orderData = JSON.parse(storedOrderId);
+} catch (error) {
+  console.error('Error al parsear el objeto de la orden:', error);
+}
+
+// Extraer solo las propiedades necesarias y formatear el mensaje con imágenes en miniatura
+const formattedMessage = orderData.items.map(item => (
+  `*[Imagen: ${item.image}]*\n💰 *Precio:* ${item.unit_price}\n🔢 *Cantidad:* ${item.quantity}\n\n`
+));
+
+// Crear el enlace de WhatsApp con el mensaje formateado
+const encodedMessage = encodeURIComponent(`*Detalles del Pedido:*\n\n${formattedMessage.join('')}`);
+const whatsappLink = `https://api.whatsapp.com/send?phone=5492213602683&text=${encodedMessage}`;
+
+// Abrir el enlace de WhatsApp en una nueva ventana o pestaña
+window.open(whatsappLink, '_blank');
+
     
     
          
