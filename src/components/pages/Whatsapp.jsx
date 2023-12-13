0e9fb33f4aa6b5cb88918d3dce52b077c4aa6845
mariade_snchz <calder07@gmail.com>
2023-12-12 23:12:38 -0300

    Formato msj Whatsapp

--- src/components/pages/Whatsapp.jsx
+++ src/components/pages/Whatsapp.jsx
@@ -7,30 +7,27 @@ import React, { useEffect } from 'react';
 //componente para enviar el carrito de compras al whatsapp del vendedor e interactuar
 const Whatsapp = () => {
   
-         let storedOrderId = localStorage.getItem("order");
-         let orderData = {};
-         
-         try {
-           orderData = JSON.parse(storedOrderId);
-         } catch (error) {
-           console.error('Error al parsear el objeto de la orden:', error);
-         }
-         
-         // Extraer solo las propiedades necesarias
-         const simplifiedOrder = {
-           items: orderData.items.map(item => ({
-             image: item.image,
-             unit_price: item.unit_price,
-             quantity: item.quantity,
-           })),
-         };
-         
-         // Crear el enlace de WhatsApp con el mensaje codificado
-         const encodedMessage = encodeURIComponent(JSON.stringify(simplifiedOrder));
-         const whatsappLink = `https://api.whatsapp.com/send?phone=5492213602683&text=${encodedMessage}`;
-         
-         // Abrir el enlace de WhatsApp en una nueva ventana o pestaña
-         window.open(whatsappLink, '_blank');
+    let storedOrderId = localStorage.getItem("order");
+    let orderData = {};
+    
+    try {
+      orderData = JSON.parse(storedOrderId);
+    } catch (error) {
+      console.error('Error al parsear el objeto de la orden:', error);
+    }
+    
+    // Extraer solo las propiedades necesarias y formatear el mensaje
+    const formattedMessage = orderData.items.map(item => (
+      `📷 *Imagen:* ${item.image}\n💰 *Precio:* ${item.unit_price}\n🔢 *Cantidad:* ${item.quantity}\n\n`
+    ));
+    
+    // Crear el enlace de WhatsApp con el mensaje formateado
+    const encodedMessage = encodeURIComponent(`*Detalles del Pedido:*\n\n${formattedMessage.join('')}`);
+    const whatsappLink = `https://api.whatsapp.com/send?phone=5492213602683&text=${encodedMessage}`;
+    
+    // Abrir el enlace de WhatsApp en una nueva ventana o pestaña
+    window.open(whatsappLink, '_blank');
+    
          
       
 
