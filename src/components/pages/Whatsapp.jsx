ef2bc49cd1390c3a1d6d98fc149e4c6a8438c9c1
mariade_snchz <calder07@gmail.com>
2023-12-14 23:03:20 -0300

    image

--- src/components/pages/Whatsapp.jsx
+++ src/components/pages/Whatsapp.jsx
@@ -14,23 +14,52 @@ const Whatsapp = () => {
     console.error('Error al parsear el objeto de la orden:', error);
   }
 
-  const sendMessageToWhatsApp = async () => {
+  // ...
+
+const sendMessageToWhatsApp = async () => {
     const formattedMessage = await Promise.all(orderData.items.map(async (item) => {
       // Obtener la URL de descarga de Firebase Storage
-      const imageRef = ref(storage, item.image);  // Ensure that item.image is the correct path in your storage
+      const imageRef = ref(storage, item.image);
       const imageUrl = await getDownloadURL(imageRef);
-
-      // Formatear el mensaje con la imagen
-      return `*[Imagen: ${item.image}]*\n💰 *Precio:* ${item.unit_price}\n🔢 *Cantidad:* ${item.quantity}\n${imageUrl}\n\n`;
+  
+      // Enviar la imagen al servidor para hacerla pública
+      const publicImageUrl = await sendImageToServer(imageUrl);
+  
+      // Formatear el mensaje con la imagen pública
+      return `*[Imagen: ${item.image}]*\n💰 *Precio:* ${item.unit_price}\n🔢 *Cantidad:* ${item.quantity}\n${publicImageUrl}\n\n`;
     }));
-
+  
     // Crear el enlace de WhatsApp con el mensaje formateado
     const encodedMessage = encodeURIComponent(`*Detalles del Pedido:*\n\n${formattedMessage.join('')}`);
     const whatsappLink = `https://api.whatsapp.com/send?phone=5492213602683&text=${encodedMessage}`;
-
+  
     // Abrir el enlace de WhatsApp en una nueva ventana o pestaña
     window.open(whatsappLink, '_blank');
   };
+  
+  // Función para enviar la imagen al servidor para hacerla pública
+  const sendImageToServer = async (imageUrl) => {
+    try {
+      const response = await fetch('https://backend-l.vercel.app/upload', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ image: imageUrl }),
+      });
+  
+      const { imageUrl: publicImageUrl } = await response.json();
+      return publicImageUrl;
+    } catch (error) {
+      console.error('Error al enviar la imagen al servidor:', error);
+      // Manejar el error según tus necesidades
+      return imageUrl; // Devuelve la URL original en caso de error
+    }
+  };
+  
+  // ...
+  
+  
 
   useEffect(() => {
     // Llamar a la función al cargar el componente
