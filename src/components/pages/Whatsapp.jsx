84c3e761322723a6b801d179562e303280720cb3
mariade_snchz <calder07@gmail.com>
2023-12-14 21:51:33 -0300

    Cart

--- src/components/pages/Whatsapp.jsx
+++ src/components/pages/Whatsapp.jsx
@@ -1,14 +1,32 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useContext } from 'react';
-import { CartContext } from "../../context/CartContext";
+// Whatsapp.jsx
+import React, { useEffect } from 'react';
+import { getDownloadURL, ref } from 'firebase/storage';
+import { db, storage } from '../../firebaseConfig';  // Ensure correct relative path
 
 const Whatsapp = () => {
-  const { cartScreenshot } = useContext(CartContext);
+  let storedOrderId = localStorage.getItem("order");
+  let orderData = {};
+
+  try {
+    orderData = JSON.parse(storedOrderId);
+  } catch (error) {
+    console.error('Error al parsear el objeto de la orden:', error);
+  }
 
   const sendMessageToWhatsApp = async () => {
+    const formattedMessage = await Promise.all(orderData.items.map(async (item) => {
+      // Obtener la URL de descarga de Firebase Storage
+      const imageRef = ref(storage, item.image);  // Ensure that item.image is the correct path in your storage
+      const imageUrl = await getDownloadURL(imageRef);
+
+      // Formatear el mensaje con la imagen
+      return `*[Imagen: ${item.image}]*\n💰 *Precio:* ${item.unit_price}\n🔢 *Cantidad:* ${item.quantity}\n${imageUrl}\n\n`;
+    }));
+
     // Crear el enlace de WhatsApp con el mensaje formateado
-    const encodedMessage = encodeURIComponent(`¡Hola! Adjunto está mi carrito de compras.`);
-    const whatsappLink = `https://wa.me/5492213602683?text=${encodedMessage}`;
+    const encodedMessage = encodeURIComponent(`*Detalles del Pedido:*\n\n${formattedMessage.join('')}`);
+    const whatsappLink = `https://api.whatsapp.com/send?phone=5492213602683&text=${encodedMessage}`;
 
     // Abrir el enlace de WhatsApp en una nueva ventana o pestaña
     window.open(whatsappLink, '_blank');
@@ -17,7 +35,7 @@ const Whatsapp = () => {
   useEffect(() => {
     // Llamar a la función al cargar el componente
     sendMessageToWhatsApp();
-  }, [cartScreenshot]); // Asegúrate de incluir cartScreenshot en las dependencias si es necesario
+  }, []);  // Agrega dependencias si es necesario
 
   return (
     <div>
