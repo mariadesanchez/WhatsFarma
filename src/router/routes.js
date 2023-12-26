ee81433240f9cd7c1a9deb17f36ed7910e468f08
mariade_snchz <calder07@gmail.com>
2023-12-25 22:23:45 -0300

    URL back

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,13 @@
+/* eslint-disable no-undef */
 import Cart from "../components/pages/cart/Cart";
-import Checkout from "../components/pages/checkout/Checkout";
 import Home from "../components/pages/home/Home";
 import ItemDetail from "../components/pages/itemDetail/ItemDetail";
 import ItemListContainer from "../components/pages/itemlist/ItemListContainer";
 import UserOrders from "../components/pages/userOrders/UserOrders";
+import Favs from "../components/pages/Favs/Favs";
+import Categoria from "../components/pages/categoria/Categoria";
+import Whatsapp from "../components/pages/Whatsapp";
+
 
 export const routes = [{
         id: "home",
@@ -35,5 +39,20 @@ export const routes = [{
         path: "/user-orders",
         Element: UserOrders,
     },
+    {
+        id: "favoritos",
+        path: "/favoritos",
+        Element: Favs,
+    },
+    {
+        id: "categoria",
+        path: "/categoria",
+        Element: Categoria,
+    },
+    {
+        id: "whatsapp ",
+        path: "/whatsapp",
+        Element: Whatsapp,
+    }
 
 ];
